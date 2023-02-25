b48414716433033bb07ddbf386fe00b1e20ddd1c
ITatRPR <andy@r-p-r.co.uk>
2023-02-25 21:35:34 +0000

    update

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -2,33 +2,20 @@ import { Box, Link, Text } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const links = {
-  github: 'https://github.com/ethereum-boilerplate/ethereum-boilerplate/',
-  forum: 'https://forum.moralis.io/',
-  moralis: 'https://moralis.io/?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate',
+  telegram: 'https://forum.moralis.io/'
 };
 
 const Footer = () => {
   return (
     <Box textAlign={'center'} w="full" p={6}>
+      
       <Text>
-        ⭐️ Please star this{' '}
-        <Link href={links.github} isExternal alignItems={'center'}>
-          boilerplate <ExternalLinkIcon />
-        </Link>
-        , every star makes us very happy!
-      </Text>
-      <Text>
-        🙋 You have questions? Ask them on the{' '}
-        <Link href={links.forum} isExternal alignItems={'center'}>
-          Moralis forum <ExternalLinkIcon />
+        Stay up to date via our Telegram{' '}
+        <Link href={links.telegram} isExternal alignItems={'center'}>
+          <ExternalLinkIcon />
         </Link>
       </Text>
-      <Text>
-        📖 Read more about{' '}
-        <Link href={links.moralis} isExternal alignItems={'center'}>
-          Moralis <ExternalLinkIcon />
-        </Link>
-      </Text>
+      
     </Box>
   );
 };
--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,62 +1,12 @@
-import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+import { Heading, VStack, Text} from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <VStack w={'full'}>
       <Heading size="md" marginBottom={6}>
-        Ethereum Boilerplate
+        AI Scanner Bot
       </Heading>
-      <List spacing={3}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Moralis authentication
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display Transactions
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Multichain Support
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Using Moralis from client-side
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Adding explorer links to balances, transactions ...
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Better responsive design
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Rainbowkit integration
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          ... and more
-        </ListItem>
-      </List>
+      <Text>The AI Scanner Bot will be able to monitor the blockchain in real time in order to identify emerging security threats and any suspicious activity, alerting users quickly to any changes they should be aware of. Additionally, it will be capable of providing a detailed analysis of the code to ensure that the contract is running optimally and meeting the highest security standards under our smart audit function, that will serve as a quick audit, rather than using third party services, that use conventional manual methods of verifying the contracts, which cost - time wise is no competition for our smart audit feature. The audit will conclude with a test score out of a 100, that will represent how safe our bot ranks the smart contract scanned. Lastly, it will be able to provide developers with guidance and advice to help them meet the security requirements in their smart contracts.</Text>
     </VStack>
   );
 };
